3638487bdb217f2515a4322d2db86c5187eb1bb2
jinsunee <jsunee.dev@gmail.com>
2021-01-02 02:38:36 +0900

    complete

--- ios/rnFirbaseExample/AppDelegate.m
+++ ios/rnFirbaseExample/AppDelegate.m
@@ -1,3 +1,4 @@
+#import <Firebase.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -27,9 +28,13 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#ifdef FB_SONARKIT_ENABLED
-  InitializeFlipper(application);
-#endif
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+  
+  #ifdef FB_SONARKIT_ENABLED
+    InitializeFlipper(application);
+  #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
